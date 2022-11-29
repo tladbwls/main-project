d4c68f015d34a34c732c4f82f936c48a15df3b78
tladbwls <zipte@naver.com>
2022-11-29 12:49:36 +0900

    1129javascript

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,43 @@
+/*************Elements colne For Mobile*********/
+const mobileMenus = document.querySelector(".mobile-menus");
+const navs = document.querySelector(".nav-lists").cloneNode(true);
+const info = document.querySelector(".info").cloneNode(true);
+
+// console.log(navs);
+
+mobileMenus.appendChild(navs);
+mobileMenus.appendChild(info);
+
+/*************Mobile Menu Toggle*********/
+const mobileBtn = document.querySelector(".mobile-btn");
+toggleMobileBtn = (e) => {
+  const target = e.currentTarget;
+  const menuHeight = mobileMenus.scrollHeight; //scroll Height: 지정 대상의 높이값을 읽어준다.
+  target.classList.toggle("active");
+
+  if (target.classList.contains("active")) {
+    target.classList.remove("not-active");
+    mobileMenus.style.height = menuHeight + "px";
+  } else {
+    target.classList.add("not-active");
+    mobileMenus.style.height = 0;
+  }
+};
+
+mobileBtn.addEventListener("click", toggleMobileBtn);
+
+/*************Header change Effect*********/
+const header = document.querySelector("#header");
+const stickyHeader = () => {
+  const scry = window.scrollY;
+  if (scry > 0) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+window.addEventListener("scroll", stickyHeader);
+
 /********** Scroll Reveal Effect **********/
 const sr = ScrollReveal({
   reset: false,
