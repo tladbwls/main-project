2eb6cde75485eb26932b1433e0c56bf22080b60e
tladbwls <zipte@naver.com>
2022-11-30 12:47:34 +0900

    1130main

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,35 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(".wrapper", { duration: 1000 });
+sr.reveal(".landing-text-box", {
+  duration: 1000,
+  origin: "right",
+  distance: "80px",
+});
+sr.reveal(".meet-text-box", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "40px",
+});
+sr.reveal(".meet-wrapper img, .feature ", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "40px",
+  interval: 200,
+});
+
+/********** Swiper slider Effect **********/
+const swiper = new Swiper(".swiper", {
+  loop: true, //슬라이더 무한 반복
+  navigation: {
+    //네비게이션
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    //슬라이더 인디케이터
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
