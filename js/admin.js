4687fb31c48d372c7b2b13126ef84e371a57085d
tladbwls <zipte@naver.com>
2022-12-21 11:57:14 +0900

    1221 admin

---
+++ js/admin.js
@@ -0,0 +1,47 @@
+//jQuery logic
+$(function () {
+  $(".upload-hidden").on("change", function () {
+    //on은 자바스크립트의 addEventListner 역할을 한다.
+    // console.log("changed");
+    let fileneme;
+    if (window.FileReader) {
+      //파일 리더 작동 확인
+      // console.log($(this));
+      fileneme = $(this)[0].files[0].name;
+      console.log(fileneme);
+    }
+
+    // console.log($(this));
+    $(this).siblings().val(fileneme);
+  });
+  $("#main-image").on("change", imgFileSelect);
+});
+
+// imgFileSelect function here...
+const imgFileSelect = (event) => {
+  const input = event.target; //변화된 입력창 타켓 저장
+  const reader = new FileReader(); //FileReader 기능 저장
+  reader.onload = function () {
+    const dataURL = reader.result; //base64로 인코딩된 값
+    const output = document.querySelector("#img");
+    output.src = dataURL;
+  };
+  reader.readAsDataURL(input.files[0]); //파일 입력이 저장된 객체의 files배열에 FileReader 기능 실행
+};
+
+//tabs code
+const btns = document.querySelectorAll(".admin-btns button");
+const panels = document.querySelectorAll(".admin-panels .panel");
+
+btns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    btns.forEach((item) => {
+      item.classList.remove("active");
+    });
+    panels.forEach((panel) => {
+      panel.style.display = "none";
+    });
+    btns[idx].classList.add("active");
+    panels[idx].style.display = "flex";
+  });
+});
